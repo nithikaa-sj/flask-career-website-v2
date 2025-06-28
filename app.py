b095be0affa238b9e75149b7c8cf51caaf0b0993
nithikaa-sj <nithikaa24@gmail.com>
2025-06-28 13:28:53 +0000

    Added connection to database

--- app.py
+++ app.py
@@ -1,40 +1,26 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
-
-JOBS = [{
-    'id': 1,
-    'title': "Data Analyst",
-    'location': "Bengaluru, India",
-    'salary': "Rs. 10,00,000"
-}, {
-    'id': 2,
-    'title': "Data Scientist",
-    'location': "Delhi, India",
-    'salary': "Rs. 15,00,000"
-}, {
-    'id': 3,
-    'title': "Frontend Engineer",
-    'location': "Remote",
-    'salary': "Rs. 12,00,000"
-}, {
-    'id': 4,
-    'title': "Backend Engineer",
-    'location': "San Francisco, USA",
-}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html", jobs=JOBS, company_name="Happy Careers")
+    jobs = load_jobs_from_db()
+    return render_template("home.html",
+                           jobs=jobs,
+                           company_name="Happy Careers")
+
 
 @app.route('/faqs')
 def faqs():
     return render_template('faq.html')
 
+
 @app.route('/features')
 def features():
     return render_template('features.html')
+
 
 @app.route('/pricing')
 def pricing():
@@ -42,13 +28,11 @@ def pricing():
 
 
 @app.route("/api/jobs")
-def list_jobs():  
-  return jsonify(JOBS)
-
-
-
+def list_jobs():
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
 
 
 if __name__ == "__main__":
-  print("I am inside if now")
-  app.run(host="0.0.0.0", debug=True)
+    print("I am inside if now")
+    app.run(host="0.0.0.0", debug=True)
---
+++ database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine, engine,text
+import os
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(db_connection_string)
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+      result = conn.execute(text("select * from jobs"))
+
+      jobs = []
+      for row in result.mappings():
+          jobs.append(dict(row))
+
+      return jobs
