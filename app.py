7ef93a8b20346f511e3adcf0d8a93f2912ee4edb
nithikaa-sj <nithikaa24@gmail.com>
2025-06-29 13:55:04 +0000

    A few new changes

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, flash
 from database import load_jobs_from_db
 from database import load_job_from_db, add_application_to_db
 from mailjet_rest import Client
 import os
+import requests
 
 mailjet = Client(auth=(os.environ['MAILJET_API_KEY'],
                        os.environ['MAILJET_SECRET_KEY']),
                  version='v3.1')
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "devkey") 
+
+
 
 
 @app.route("/")
@@ -84,8 +88,6 @@ def apply_to_json_job(id):
     return jsonify(data)
 
 
-
-
 @app.route("/job/<id>/apply", methods=["POST"])
 def apply_to_job(id):
     job = load_job_from_db(id)
@@ -93,6 +95,25 @@ def apply_to_job(id):
     # send an email
     # display an acknowledgemnet
     data = request.form
+
+    # 1. Get hCaptcha token from form
+    hcaptcha_response = data.get("h-captcha-response")
+    secret = os.environ.get(
+        "HCAPTCHA_SECRET_KEY")  # set this in Render Environment tab
+
+    # 2. Verify with hCaptcha server
+    hcaptcha_verify_url = "https://hcaptcha.com/siteverify"
+    hcaptcha_data = {"secret": secret, "response": hcaptcha_response}
+
+    result = requests.post(hcaptcha_verify_url, data=hcaptcha_data).json()
+
+    # 3. Check success
+    if not result.get("success"):
+        print("hCaptcha failed:", result)
+        # If hCaptcha fails, redirect or show error
+        flash("hCaptcha verification failed. Please try again.")
+        return redirect(request.referrer or "/")
+
     add_application_to_db(id, data)
 
     send_email(
